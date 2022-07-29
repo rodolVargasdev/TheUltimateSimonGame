1ef1c3c606fce1c67732a4d56adc3bbe978dbb89
hawokdev <70559204+hawokdev@users.noreply.github.com>
2022-07-29 13:56:01 -0600

    Finished the css and html elements, the funcionatally is not ready yet

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("hola");
+function bolsasParaDulces(Ndulces, Kbolsillos, Mdulces)
+{
+    let totalDulcesBolsillo = Kbolsillos * Mdulces;
+    let cantidadBolsas = Ndulces/totalDulcesBolsillo;
+    return Math.ceil(cantidadBolsas);
+}
+
+console.log(bolsasParaDulces(6, 2, 3));
+console.log(bolsasParaDulces(3, 1, 2));
+console.log(bolsasParaDulces(8, 4, 1));
+console.log(bolsasParaDulces(25, 4, 2));
